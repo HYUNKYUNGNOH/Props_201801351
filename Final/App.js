7aa5d6f959a71b8275f700bea4551eb91b8ab094
hyunkyungnoh <shgusrud0910@naver.com>
2022-11-23 20:06:41 +0900

    add box

--- Final/App.js
+++ Final/App.js
@@ -1,26 +1,32 @@
 import React from "react";
 import { Button, View, ScrollView, StyleSheet, Text, Image, ImageBackground } from "react-native";
 import Constants from 'expo-constants';
+import { useState } from "react";
+
 const App = () => {
+
+  const flexDirections = ['row', 'rew-reverse', 'column', 'column-reverse'];
+
+  const [flexDirection, setFlexDirection] = useState(0);
+
+  const Square = () => {
+    const sqStyle = {
+      width: 50,
+      height: 50,
+      backgroundColor:randomHexColor(),
+    };
+    return <View style={sqStyle} />;
+  };
+
+  const [squares, setSquares] = useState([Square(), Square(), Square()]);
+
+
   return (<>
     <View style={{paddingTop:Constants.statusBarHeight}}></View>
     <View style={[styles.container, styles.playingSpace]}>
-      <Text>이름: 노낑깡</Text>
-      <Text>나이: 1살</Text>
-      
-      <Image source={require('./assets/favicon.png')}/>
-      <ImageBackground
-        style={{width:200, height:200}}
-        source={{
-          uri: 'https://postfiles.pstatic.net/MjAyMjExMjNfMjY3/MDAxNjY5MTk5OTU3MjM1.89XEFOF2hcbL73PQ_kYQFl1WmRMvVO4vcwwerO6_1Jog.oGz08RdQqYCdTs_QkVfFF7VeHHg5Qd6hIui1TSkB18Qg.JPEG.shgusrud0910/IMG_5484.jpg?type=w966',
-        }}
-      > 
-        <Text>
-        몰티즈는 몰타 섬이 고향으로 알려진 소형견으로 앙증맞은 외모와 애교 넘치는 성격, 흰 털로 많은 사랑을 받는 반려견이다. 한국에서 가장 많은 사람들이 키우는 반려견으로 ‘국민 강아지’로 통하기도 한다. 흰색 털이 변색될 수 있으며 특히 눈가의 털색이 갈색으로 변색될 수 있는데 이는 눈물자국 때문이다. 눈물자국을 방치하면 세균이 증식해 피부병의 원인이 될 수 있다. 털을 자주 관리해주지 않으면 엉키거나 끝이 갈라지는 등의 문제가 생길 수도 있다. 활발한 성격 때문에 산책을 좋아한다. 다정다감한 성격 때문에 배변 교육 등 반려견 교육도 잘 받을 수 있다.
-        </Text>
-      
-      </ImageBackground>
+      { squares.map(elem => elem) }      
     </View>
+
     <ScrollView style={[styles.container]}>
       <View style = {[styles.controlSpace]}>
         <View style={[styles.buttonView]}>
@@ -54,8 +60,8 @@ const App = () => {
             onPress={() => console.log("DELETE SQUARE")}/>
         </View>
       </View>
-
-      </ScrollView>
+      
+    </ScrollView>
   </>);
 }
 const styles = StyleSheet.create({
@@ -63,8 +69,8 @@ const styles = StyleSheet.create({
     height: '50%'
   },
   playingSpace: {
-    backgroundColor: 'pink',
-    borderColor: 'skyblue',
+    backgroundColor: 'white',
+    borderColor: 'blue',
     borderWidth: 3
   },
   controlSpace: {
@@ -77,4 +83,11 @@ const styles = StyleSheet.create({
     padding:10
   }
 });
+
+const randomHexColor = () => {
+  return '#000000'.replace(/0/g, () => {
+    return (~~(Math.random() * 16)).toString(16);
+  });
+};
+
 export default App;
